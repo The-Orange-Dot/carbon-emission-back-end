487962226bee8cbbc9272d092d5e819b5dc6086a
katroll <kroll256@GMAIL.COM>
2022-01-21 14:40:18 -0500

    application controllers work

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,4 +4,21 @@ class User < ActiveRecord::Base
   has_many :shipping_histories
   has_many :vehicle_histories
   
+
+  def flightHistory
+    self.flight_histories
+  end
+
+  def shippingHistory
+    self.shipping_histories
+  end
+
+  def vehicleHistory
+    self.vehicle_histories
+  end
+
+  def electricityHistory
+    self.electricity_histories
+  end
+  
 end
