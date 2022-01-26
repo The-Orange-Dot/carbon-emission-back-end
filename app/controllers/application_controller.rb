865be925da33404b51ff408b9a1ea6b8384542f7
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-26 14:02:50 +0900

    user info update added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,4 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # i left this in here because it relates to the histories more
-  # then it relates to the user
-  patch "/user/:id" do
-    user = User.find(params[:id])  
-  
-    user.update(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      password: params[:password],
-      email: params[:email],
-      image: params[:image]
-    )
-  end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
     User.all.to_json
   end
 
+  patch "/users/update_info/:id" do
+    user = User.find(params[:id])  
+    user.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      password: params[:password],
+      email: params[:email],
+      image: params[:image]
+    )
+    user.to_json
+  end
+
 end
